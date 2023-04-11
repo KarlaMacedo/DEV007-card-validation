7c8756909dad882e43917b3e12a91788cbdd241f
KarlaMacedo <karlapaolamacedo@gmail.com>
2023-04-11 17:28:43 -0600

    CSS,HTML Y JSS (poner número de tarjeta y sólo mostrar últimos 4 dígitos al dar click en validar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import validator from './validator.js';
 
+document.getElementById ("buttonCreditNumber").addEventListener ("click", function () {
+        let mask = validator.maskify (document.getElementById("inputCreditNumber").value);
+        return document.getElementById("inputCreditNumber").value = mask
+    }) 
+
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,16 @@
 const validator = {
-  // ...
+  isValid: function () {
+    let reverseCardNumber = reverse ()
+
+  },
+
+  maskify (number) {
+    let numbersHide = number.slice (0 , -4);
+    let numbersShow = number.slice (-4);
+    let maskNumbers = numbersHide.replace (/./g,  "*" );
+    return maskNumbers + numbersShow
+  }
+
 };
 
 export default validator;
